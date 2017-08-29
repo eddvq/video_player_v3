2b7831d600b5be1b044eb744ea22362a1f5f1aa8
eddvq <eddyvzqz@gmail.com>
2017-08-28 18:30:14 -0700

    Text Highlight Implemented

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 /*jshint esversion: 6 */
+
+const video = document.getElementById('video');
+const spanData = document.querySelectorAll('div span');
+
+video.addEventListener('timeupdate', (e) => {
+  for (let i = 0; i < spanData.length; i++){
+
+    let span = spanData[i];
+    let dataStart = span.getAttribute('data-start');
+    let dataEnd = span.getAttribute('data-end');
+
+    let currentVidTime = video.getCurrentTime();
+
+    if(currentVidTime > dataStart && currentVidTime <= dataEnd){
+      span.className = "highlight";
+    } else {
+      span.className = '';
+    }
+  }
+});
